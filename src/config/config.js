c3e704b62c1b195aa9806807ee141fef4177aeaf
mahesh <mahesh@blocklogy.org>
2020-09-23 13:38:53 +0530

    contract addresses changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,9 @@
 module.exports = {
   baseUrl: process.env.REACT_APP_BASE_URL,
   nodeUrl: process.env.REACT_APP_NODE_URL,
-  nrtAddress:
-    process.env.REACT_APP_NRT_ADDRESS ||
-    '0xcA4d0578c5e07F0964C7E7ccc87E606A234625b8',
-  timeAllyAddress:
-    process.env.REACT_APP_TIMEALLY_ADDRESS ||
-    '0x89309551Fb7AbaaB85867ACa60404CDA649751d4',
-  validatorsStakesAddress:
-    process.env.REACT_APP_VALIDATORS_STAKES_ADDRESS ||
-    '0x8418249278d74D46014683A8029Fd6fbC88482a1',
-  reversePlasmaAddress:
-    process.env.REACT_APP_REVERSE_PLASMA_ADDRESS ||
-    '0x3bEb087e33eC0B830325991A32E3F8bb16A51317',
-  plasmaAddress:
-    process.env.REACT_APP_PLASMA_ADDRESS ||
-    '0x1da7f478683206820DE9C9c76882F93A725A39F2',
+  nrtAddress: '0x89309551Fb7AbaaB85867ACa60404CDA649751d4',
+  timeAllyAddress: '0x7F87f9830baB8A591E6f94fd1A47EE87560B0bB0',
+  validatorsStakesAddress: '0xE14D14bd8D0E2c36f5E4D00106417d8cf1000e22',
+  reversePlasmaAddress: '0x3bEb087e33eC0B830325991A32E3F8bb16A51317',
+  plasmaAddress: '0x1c5b6e1ff599D1aCEd9cfCE73efab34f0688977e',
 };
