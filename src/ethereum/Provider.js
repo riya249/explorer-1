42ed629e1c7afab7ae4ae716408d65af6e7f245e
maheshrapelli141 <maheshrapelli141@gmail.com>
2020-10-27 13:13:06 +0530

    providerESN issue fixed

--- src/ethereum/Provider.js
+++ src/ethereum/Provider.js
@@ -2,18 +2,17 @@ const { ethers } = require('ethers');
 const { nodeUrl } = require('../config/config');
 const { es } = require('eraswap-sdk');
 
-const providerESN = new es.CustomProvider('testnet');
+export const providerESN = new es.CustomProvider('testnet');
 // const providerESN = new ethers.providers.JsonRpcProvider('https://node2.testnet.eraswap.network');
 
-const providerEth = ethers.getDefaultProvider('rinkeby', {
+export const providerEth = ethers.getDefaultProvider('rinkeby', {
   infura: 'b915fe11a8ab4e73a3edba4c59d656b2',
 });
 
-const surveyInstance = es.typechain.ESN.BuildSurveyFactory.connect(es.addresses[process.env.REACT_APP_NODE_ENV].ESN.buildSurvey,providerESN);
+export const surveyInstance = es.typechain.ESN.BuildSurveyFactory.connect(es.addresses[process.env.REACT_APP_NODE_ENV].ESN.buildSurvey,providerESN);
 
-module.exports = {
-  providerEth,
-  providerESN,
-  surveyInstance,
-  surveyInstance
-};
+// module.exports = {
+//   providerEth,
+//   providerESN,
+//   surveyInstance,
+// };
